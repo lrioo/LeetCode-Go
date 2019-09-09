d0495429fa240d04447609fbddc23e6a68460630
CestLaVieRio <979491626@qq.com>
2019-09-09 23:17:23 +0800

    problem 14 & 20

---
+++ 0014 Longest Common Prefix ☆/longest_common_prefix.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Input := []string{"flower", "flow", "flowll"}
+	//Output = "fl"
+	fmt.Println(longestCommonPrefix(Input))
+
+	Input = []string{"dog", "racecar", "car"}
+	//Output = ""
+	fmt.Println(longestCommonPrefix(Input))
+
+	Input = []string{"aaa", "aa", "a"}
+	//Output = ""
+	fmt.Println(longestCommonPrefix(Input))
+}
+
+func longestCommonPrefix(strs []string) string {
+	var prefix string
+
+	for i := 0; i < len(strs); i++ {
+		if i == 0 {
+			prefix = strs[0]
+			continue
+		}
+
+		var j int
+		for j = 0; j < len(prefix) && j < len(strs[i]); j++ {
+			if prefix[j] != strs[i][j] {
+				if j == 0 {
+					return ""
+				}
+				break
+			}
+		}
+		prefix = prefix[:j]
+	}
+
+	return prefix
+}
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+If there is no common prefix, return an empty string "".
+如果不存在公共前缀，返回空字符串 ""。
+
+
+Example 1:
+示例 1:
+
+  Input: ["flower","flow","flight"]
+  输入: ["flower","flow","flight"]
+
+  Output: "fl"
+  输出: "fl"
+
+
+Example 2:
+示例 2:
+
+  Input: ["dog","racecar","car"]
+  输入: ["dog","racecar","car"]
+
+  Output: ""
+  输出: ""
+  Explanation: There is no common prefix among the input strings.
+  解释: 输入不存在公共前缀。
+
+
+Note:
+说明:
+
+  All given inputs are in lowercase letters a-z.
+  所有输入只包含小写字母 a-z 。
+*/
---
+++ 0020 Valid Parentheses ☆/valid_parentheses.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Input := "()"
+	//Output = true
+	fmt.Println(isValid(Input))
+
+	Input = "()[]{}"
+	//Output = true
+	fmt.Println(isValid(Input))
+
+	Input = "(]"
+	//Output = false
+	fmt.Println(isValid(Input))
+
+	Input = "([)]"
+	//Output = false
+	fmt.Println(isValid(Input))
+
+	Input = "{[]}"
+	//Output = true
+	fmt.Println(isValid(Input))
+}
+
+func isValid(s string) bool {
+	return false
+}
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+
+An input string is valid if:
+有效字符串需满足：
+
+  Open brackets must be closed by the same type of brackets.
+  左括号必须用相同类型的右括号闭合。
+
+  Open brackets must be closed in the correct order.
+  左括号必须以正确的顺序闭合。
+
+  Note that an empty string is also considered valid.
+  注意空字符串可被认为是有效字符串。
+
+
+Example 1:
+示例 1:
+
+  Input: "()"
+  输入: "()"
+
+  Output: true
+  输出: true
+
+
+Example 2:
+示例 2:
+
+  Input: "()[]{}"
+  输入: "()[]{}"
+
+  Output: true
+  输出: true
+
+
+Example 3:
+示例 3:
+
+  Input: "(]"
+  输入: "(]"
+
+  Output: false
+  输出: false
+
+
+Example 4:
+示例 4:
+
+  Input: "([)]"
+  输入: "([)]"
+
+  Output: false
+  输出: false
+
+
+Example 5:
+示例 5:
+
+  Input: "{[]}"
+  输入: "{[]}"
+
+  Output: true
+  输出: true
+*/
