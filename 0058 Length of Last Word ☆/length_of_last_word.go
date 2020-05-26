d06e0d3f07aeeff75e194a04b6e82eef335a6a62
Rio <979491626@qq.com>
2020-05-26 18:24:49 +0800

    problem 58

---
+++ 0058 Length of Last Word ☆/length_of_last_word.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word
+(last word means the last appearing word if we loop from left to right) in the string.
+给定一个仅包含大小写字母和空格' '的字符串s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+
+If the last word does not exist, return 0.
+如果不存在最后一个单词，请返回0。
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+说明：一个单词是指仅由字母组成、不包含任何空格字符的最大子字符串。
+
+Example:
+示例:
+
+  Input: "Hello World"
+  输入: "Hello World"
+
+  Output: 5
+  输出: 5
+*/
