e819f260087cfb73c4bd22a39e7b1b3f46e46252
Rio <979491626@qq.com>
2020-06-12 19:24:28 +0800

    problem 118

--- 0118 Pascal's Triangle ☆/pascals_triangle.go
+++ 0118 Pascal's Triangle ☆/pascals_triangle.go
@@ -10,38 +10,25 @@ func generate(numRows int) [][]int {
 
 /*
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
-
-
-In Pascal's triangle, each number is the sum of the two numbers directly above it.
-
-Example:
-
-Input: 5
-Output:
-[
-     [1],
-    [1,1],
-   [1,2,1],
-  [1,3,3,1],
- [1,4,6,4,1]
-]
-
-
 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
 
-
-
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
 在杨辉三角中，每个数是它左上方和右上方的数的和。
 
+Example:
 示例:
 
-输入: 5
-输出:
-[
-     [1],
-    [1,1],
-   [1,2,1],
-  [1,3,3,1],
- [1,4,6,4,1]
-]
+  Input: 5
+  输入: 5
+
+  Output:
+  输出:
+
+      [
+           [1],
+          [1,1],
+         [1,2,1],
+        [1,3,3,1],
+       [1,4,6,4,1]
+     ]
 */
