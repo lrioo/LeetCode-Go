c3949c4f220555828402f0d54e01ff28ed4b510a
rio <979491626@qq.com>
2020-07-05 16:49:23 +0800

    problem 118

--- 0118 Pascal's Triangle ☆/pascals_triangle.go
+++ 0118 Pascal's Triangle ☆/pascals_triangle.go
@@ -18,13 +18,12 @@ func generate(numRows int) [][]int {
 	rows := make([][]int, numRows)
 	rows[0] = []int{1}
 	for i := 1; i < numRows; i++ {
-		row := make([]int, i+1)
-		row[0], row[i] = 1, 1
+		rows[i] = make([]int, i+1)
+		rows[i][0], rows[i][i] = 1, 1
 
 		for j := 1; j < i; j++ {
-			row[j] = rows[i-1][j-1] + rows[i-1][j]
+			rows[i][j] = rows[i-1][j-1] + rows[i-1][j]
 		}
-		rows[i] = row
 	}
 	return rows
 }
