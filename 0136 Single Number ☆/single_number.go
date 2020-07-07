50cd3feecf2ef369431d8b9fa3a75ce771c2ec11
Rio <979491626@qq.com>
2020-07-07 10:51:51 +0800

    problem 136

--- 0136 Single Number ☆/single_number.go
+++ 0136 Single Number ☆/single_number.go
@@ -1,11 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	Input := []int{2, 2, 1}
+	//Output := 1
+	fmt.Println(singleNumber(Input))
+
+	Input = []int{4, 1, 2, 1, 2}
+	//Output := 4
+	fmt.Println(singleNumber(Input))
 }
 
 func singleNumber(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		nums[0] ^= nums[i]
+	}
 
+	return nums[0]
 }
 
 /*
