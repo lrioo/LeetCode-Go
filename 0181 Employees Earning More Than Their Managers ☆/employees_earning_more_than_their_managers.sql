b114b5c014e220c52a34299febc1649ff661ec4f
Rio <979491626@qq.com>
2020-07-10 10:56:20 +0800

    problem 181

--- 0181 Employees Earning More Than Their Managers ☆/employees_earning_more_than_their_managers.sql
+++ 0181 Employees Earning More Than Their Managers ☆/employees_earning_more_than_their_managers.sql
@@ -22,6 +22,16 @@ values ('4', 'Max', '90000', 'None');
 
 -- Write your MySQL query statement below
 
+SELECT E1.Name AS Employee
+FROM Employee E1,
+     Employee E2
+WHERE E1.ManagerId = E2.Id
+  AND E1.Salary > E2.Salary;
+
+SELECT E1.Name AS Employee
+FROM Employee E1
+         JOIN Employee E2 ON E1.ManagerId = E2.Id
+WHERE E1.Salary > E2.Salary;
 
 
 /*
